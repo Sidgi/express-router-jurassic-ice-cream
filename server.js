5acec8d3bdd759488183cc9c22eaa651865ef67b
Sidgi Samanduyev <samanduyev@gmail.com>
2019-05-04 21:37:53 -0400

    completed dino by name

--- server.js
+++ server.js
@@ -30,15 +30,41 @@ app.get('/flavors', async(req,res)=>{
 
 // Create a route that displays a single dino by id Ex: /dinos/id/1 should display json of the dino with an id of 1
 
-
+app.get('/dinos/:id', async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const dino = await Dinosaur.findByPk(id);
+        res.send(dino);
+    }catch(e){
+        res.status(500).json({msg:e.message})
+    }
+})
 
 // Create a route that displays a single dino by name Ex: /dinos/name/barney should display json of barney the dinosaur
 
-
+app.get('/dino/:name',async(req,res)=>{
+    try{
+        const name = req.params.name;
+        const dinoName = await Dinosaur.findAll({
+            where:{
+                name:name
+            }
+        });
+        res.send(dinoName)
+    }catch(e){
+        res.json(e.message)
+    }
+})
 
 // Create a route that displays a single flavor by id Ex: /flavors/id/1 should display json of the flavor with an id of 1
 
-
+get.app('/flavors/:id',(req,res)=>{
+    try{
+        const flavorId = req.params.id;
+    }catch(e){
+        res.status.json({message:e.message})
+    }
+})
 
 // Create a route that displays a single flavor by name Ex: /flavors/name/lemon should display json of the lemon flavor
 
